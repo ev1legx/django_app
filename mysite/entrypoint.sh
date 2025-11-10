ef00100857b11e22971466e8b09f3f197587d637
Дмитрий Сениченко <modya1997@mail.ru>
2025-11-10 11:20:10 +0300

    миграции

--- mysite/entrypoint.sh
+++ mysite/entrypoint.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
+
+echo "Ожидание готовности базы данных..."
+
+while ! nc -z ${DB_HOST:-localhost} ${DB_PORT:-5432}; do
+  echo "База данных недоступна, ждем 1 секунду..."
+  sleep 1
+done
+
 echo "Применение миграций..."
 python manage.py migrate --noinput
+
+echo "Сборка статики..."
+python manage.py collectstatic --noinput
+
 echo "Запуск Gunicorn..."
 exec gunicorn mysite.wsgi:application --bind 0.0.0.0:8000
